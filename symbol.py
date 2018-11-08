779754386202237425b5ee25ac8d76c5e851c866
Leo <geniusshiun@gmail.com>
2018-11-08 15:33:05 +0800

    Add symbol
    Add symbol test python code.

---
+++ symbol.py
@@ -0,0 +1,36 @@
+def strQ2B(ustring):
+    """把字串全形轉半形"""
+    rstring = []
+    for uchar in ustring:
+        inside_code=ord(uchar)
+        if inside_code==0x3000:
+            inside_code=0x0020
+        else:
+            inside_code-=0xfee0
+        if inside_code<0x0020 or inside_code>0x7e:   #轉完之後不是半形字元返回原來的字元
+            rstring.append(uchar)
+        else:
+            rstring.append(chr(inside_code))
+
+    return ''.join(rstring)
+
+def strB2Q(ustring):
+    """把字串半形轉全形"""
+    rstring = []
+    for uchar in ustring:
+        inside_code=ord(uchar)
+        if inside_code<0x0020 or inside_code>0x7e:   #不是半形字元就返回原來的字元
+            rstring.append(uchar)
+            continue
+        if inside_code==0x0020: #除了空格其他的全形半形的公式為:半形=全形-0xfee0
+            inside_code=0x3000
+        else:
+            inside_code +=0xfee0
+        rstring.append(chr(inside_code))
+    return ''.join(rstring)
+
+if __name__ == '__main__':
+    newword = strB2Q('。天abc123!@#$%^&*()_+[]\/.,~`}{":?><')
+    print(newword)
+    print(strQ2B(newword))
+    print(strQ2B('ＡＢＣ！！！$@#＠＃＄＠＃％）（＠＊!!１２３４５!!'))
